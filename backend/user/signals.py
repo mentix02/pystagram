4766dd06779a19bf941ded352e9ca14242a247e6
mentix02 <manan.yadav02@gmail.com>
2022-10-17 15:31:28 +0530

    Added m2m signal for deleting bookmarks after unfollow and type hints

--- backend/user/signals.py
+++ backend/user/signals.py
@@ -1,12 +1,21 @@
-from django.conf import settings
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 
 from rest_framework.authtoken.models import Token
 
+from user.models import User
+from bookmark.models import Bookmark
+
 
 # noinspection PyUnusedLocal
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance: User = None, created: bool = False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+# noinspection PyUnusedLocal
+@receiver(m2m_changed, sender=User._follows.through)
+def delete_bookmarks(sender, instance: User = None, action=None, pk_set=None, **kwargs):
+    if action == 'post_remove':
+        Bookmark.objects.filter(user=instance, post__user_id__in=pk_set).delete()
